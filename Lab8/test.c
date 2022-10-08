0c7891291f1bdbb3173a8e512628f9ed4f41f683
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-10-09 00:41:53 +0700

    cv

--- Lab8/test.c
+++ Lab8/test.c
@@ -1,61 +1,79 @@
 #include <stdio.h>
 #include <windows.h>
-#include <conio.h>
-void gotoxy(int x, int y)
+#include <time.h>
+#define scount 80
+#define screen_x 80
+#define screen_y 25
+HANDLE wHnd;
+CHAR_INFO consoleBuffer[screen_x * screen_y];
+COORD bufferSize = { screen_x,screen_y };
+COORD characterPos = { 0,0 };
+SMALL_RECT windowSize = { 0,0,screen_x - 1,screen_y - 1 };
+COORD star[scount];
+
+int setConsole(int x, int y)
 {
-    COORD c = {x, y};
-    SetConsoleCursorPosition(
-        GetStdHandle(STD_OUTPUT_HANDLE), c);
+	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
+	SetConsoleScreenBufferSize(wHnd, bufferSize);
+	return 0;
 }
-void setcursor(boolean visible)
+void clear_buffer()
 {
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO lpCursor;
-    lpCursor.bVisible = visible;
-    lpCursor.dwSize = 20;
-    SetConsoleCursorInfo(console, &lpCursor);
+	for (int i = 0; i < screen_x * screen_y; i++) {
+		consoleBuffer[i].Char.AsciiChar = ' ';
+		consoleBuffer[i].Attributes = 7;
+	}
 }
-void setcolor(int fg, int bg)
+void fill_buffer_to_console()
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+	WriteConsoleOutputA(wHnd, consoleBuffer, bufferSize, characterPos,
+		&windowSize);
 }
-void draw_ship(int x, int y)
+void init_star()
 {
-    gotoxy(x, y);
-    setcolor(2, 4);
-    printf(" <-0-> ");
+	for (int i = 0; i < scount; i++) {
+		star[i].X = rand() % 80;
+		star[i].Y = rand() % 25;
+	}
 }
-void erase_ship(int x, int y)
+void star_fall()
 {
-    gotoxy(x, y);
-    setcolor(0, 0);
-    printf("       ");
+	int i;
+	for (i = 0; i < scount; i++) {
+		if (star[i].Y >= screen_y - 1) {
+			star[i] = { (rand() % screen_x),0 };
+		}
+		else {
+			star[i] = { star[i].X , star[i].Y + 1 };
+		}
+	}
+
+}
+
+void fill_star_to_buffer()
+{
+
+	for (int i = 0; i < scount; i++) {
+		consoleBuffer[star[i].X + star[i].Y*screen_x].Char.AsciiChar = '*';
+		consoleBuffer[star[i].X + star[i].Y * screen_x].Attributes = 7;
+	}
 }
 int main()
 {
-    setcursor(0);
-    char ch = '.';
-    int x = 38, y = 20;
-    draw_ship(x, y);
-    do
-    {
-        if (kbhit())
-        {
-            ch = getch();
-            if (ch == 'a')
-            {
-                erase_ship(x, y);
-                draw_ship(--x, y);
-            }
-            if (ch == 'd')
-            {
-                erase_ship(x, y);
-                draw_ship(++x, y);
-            }
-            fflush(stdin);
-        }
-        Sleep(100);
-    } while (ch != 'x');
-    return 0;
+	int i;
+	srand(time(NULL));
+	setConsole(screen_x, screen_y);
+	init_star();
+	i = 0;
+	while (i < 1000)
+	{
+		star_fall();
+		clear_buffer();
+		fill_star_to_buffer();
+		fill_buffer_to_console();
+		Sleep(200);
+		i++;
+	}
+	return 0;
 }
